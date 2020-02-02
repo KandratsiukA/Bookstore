87c24b9ecf6e44a7dea4e2f0e34433375cdf8fa2
Kandratsiuk <anastasiya.kondratiuk@yandex.by>
2020-02-02 09:48:10 +0100

    user controller

---
+++ controllers/UserController.js
@@ -0,0 +1,56 @@
+const express=require("express");
+const router=express.Router();
+const User = require("../models/user");
+var bodyParser = require('body-parser');
+var VerifyToken = require(__root + 'auth/VerifyToken');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+
+// CREATES A NEW USER
+router.post('/', function (req, res) {
+    User.create({
+            username : req.body.username,
+            password : req.body.password
+        }, 
+        function (err, user) {
+            if (err) return res.status(500).send("There was a problem adding the information to the database.");
+            res.status(200).send(user);
+        });
+});
+
+// RETURNS ALL THE USERS IN THE DATABASE
+router.get('/', function (req, res) {
+    User.find({}, function (err, users) {
+        if (err) return res.status(500).send("There was a problem finding the users.");
+        res.status(200).send(users);
+    });
+});
+
+// GETS A SINGLE USER FROM THE DATABASE
+router.get('/:id', VerifyToken, function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+        res.status(200).send(user);
+    });
+});
+
+// UPDATES A SINGLE USER IN THE DATABASE
+router.put('/:id', VerifyToken, function (req, res) {
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+        if (err) return res.status(500).send("There was a problem updating the user.");
+        if (!user) return res.status(404).send("No user found.");
+        res.status(200).send(user);
+    });
+});
+
+// DELETES A USER FROM THE DATABASE
+router.delete('/:id', VerifyToken, function (req, res) {
+    User.findByIdAndRemove(req.params.id, function (err, user) {
+        if (err) return res.status(500).send("There was a problem deleting the user.");
+        if (!user) return res.status(404).send("No user found.");
+        res.status(200).send("User: "+ user.name +" was deleted.");
+    });
+});
+
+module.exports = router;
